7e362e5a5048ec8f66611659222ccf7fbb5c34a0
jsutlive <jsutlive@gmail.com>
2023-07-06 03:33:49 -0400

    bugfix

--- tools/gensln.py
+++ tools/gensln.py
@@ -5,8 +5,8 @@ if(globals.is_windows()):
 	subprocess.call(["cmd.exe", "/c", "premake\\premake5", "vs2022"])
 
 if(globals.is_linux()):
-	subprocess.call(["cmd.exe", "/c", "premake\\premake5.linux", "gmake2"])
+	subprocess.call(["premake/premake5.linux", "gmake2"])
 
 if(globals.is_mac()):
-	subprocess.call(["cmd.exe", "/c", "premake\\premake5.macosx", "gmake2"])
-	subprocess.call(["cmd.exe", "/c", "premake\\premake5.macosx", "xcode4"])
+	subprocess.call(["premake/premake5.osx", "gmake2"])
+	subprocess.call(["premake/premake5.osx", "xcode4"])
