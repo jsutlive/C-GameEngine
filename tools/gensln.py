f6859ff508f475ea27afb15435f27a6fcf269949
jsutlive <jsutlive@gmail.com>
2023-07-06 02:47:23 -0400

    Toolchain changes

--- tools/gensln.py
+++ tools/gensln.py
@@ -1,3 +1,12 @@
 import subprocess
+import globals
 
-subprocess.call(["cmd.exe", "/c", "premake\\premake5", "vs2022"])
+if(globals.is_windows()):
+	subprocess.call(["cmd.exe", "/c", "premake\\premake5", "vs2022"])
+
+if(globals.is_linux()):
+	subprocess.call(["cmd.exe", "/c", "premake\\premake5.linux", "gmake2"])
+
+if(globals.is_mac()):
+	subprocess.call(["cmd.exe", "/c", "premake\\premake5.macosx", "gmake2"])
+	subprocess.call(["cmd.exe", "/c", "premake\\premake5.macosx", "xcode4"])
---
+++ tools/globals.py
@@ -0,0 +1,19 @@
+V_MAJOR = 1
+V_MINOR = 0
+
+import sys, platform
+PLATFORM = sys.platform
+
+for x in platform.uname():
+	if "microsoft" in x.lower():
+		PLATFORM = "windows"
+		break
+
+def is_windows():
+	return PLATFORM == "windows"
+
+def is_linux():
+	return PLATFORM == "linux"
+
+def is_mac():
+	return PLATFORM == "mac"
